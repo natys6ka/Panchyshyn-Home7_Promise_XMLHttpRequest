5026d9fa500596bbd916598d1e217536b3371ffe
natys6ka <natalkapni.bc@gmail.com>
2020-10-08 12:25:26 +0300

    Second commit

--- script.js
+++ script.js
@@ -6,4 +6,21 @@
 4.	Піти на роботу
  */
 
- 
+
+
+ /* Потрібно за допомогою XMLHttpRequest витягнути дані 
+ з OMDb API використовуючи Promise. 
+Потрібно в промісі відправляти запит на сервер 
+по імені а також по ІД.
+1.	Витягуємо дані по імені фільму
+2.	Дані розпарсуємо на сторінці
+3.	При натисканні на More Details має відкритися модалка з усіма даними про той фільм.
+4.	Все має працювати.
+ */
+
+document.getElementById('search').addEventListener('click', function(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=fb18a1ae', false);
+    xhr.send();
+    console.log(JSON.parse(xhr.responseText));
+ })
